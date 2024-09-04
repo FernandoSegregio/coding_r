6287146937683f839b4adaaf946527beb168ede8
FernandoSegregio <segregio@gmail.com>
2024-09-03 21:02:19 -0300

    update gitignore

---
+++ gitignore.R
@@ -0,0 +1,16 @@
+# Ignorar arquivos temporários do R
+*.Rhistory
+*.Rapp.history
+.Rproj.user/
+  
+# Ignorar arquivos de dados
+data/*.csv
+
+# Ignorar diretórios de pacotes e dependências
+.R/library/
+  .renv/
+  
+# Ignorar arquivos de configuração do RStudio
+.Rproj.user/
+  
+env*  
